ed0b7cdc410ededec6e71102e4fc3d8ce05a9536
unknown <farhanraa@gmail.com>
2018-05-18 15:16:14 +0700

    deploy12

--- laravel/app/Http/Controllers/LoginController.php
+++ laravel/app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use DB;
 
 class LoginController extends Controller
 {
@@ -28,6 +29,8 @@ class LoginController extends Controller
     	}
 
     	else{
+            
+            echo DB::table('users')->get();
     		echo "Email atau password salah";
     		return view('login');
     	}
