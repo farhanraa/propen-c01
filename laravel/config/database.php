f431cffe2a8cab12f9d470f9519d8901e0442a9e
unknown <farhanraa@gmail.com>
2018-05-18 16:21:04 +0700

    tes

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -1,5 +1,22 @@
 <?php
+
+$DATABASE_URL = parse_url(getenv("DATABASE_URL"));
+
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | PDO Fetch Style
+    |--------------------------------------------------------------------------
+    |
+    | By default, database results will be returned as instances of the PHP
+    | stdClass object; however, you may desire to retrieve records in an
+    | array format for simplicity. Here you can tweak the fetch style.
+    |
+    */
+
+    //'fetch' => PDO::FETCH_OBJ,
+
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -10,7 +27,9 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
-    'default' => env('DB_CONNECTION'),
+
+    'default' => 'pgsql',
+
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -26,49 +45,23 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
+
     'connections' => [
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-        ],
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
-        'pgsql' => [
+
+
+        'pgsql' => array(
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'database' => substr($DATABASE_URL["path"], 1),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'HANRA\SQLEXPRESS'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
+        ),
 
-        ],
     ],
+
     /*
     |--------------------------------------------------------------------------
     | Migration Repository Table
@@ -79,7 +72,9 @@ return [
     | the migrations on disk haven't actually been run in the database.
     |
     */
+
     'migrations' => 'migrations',
+
     /*
     |--------------------------------------------------------------------------
     | Redis Databases
@@ -90,13 +85,18 @@ return [
     | such as APC or Memcached. Laravel makes it easy to dig right in.
     |
     */
+
     'redis' => [
-        'client' => 'predis',
+
+        'cluster' => false,
+
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
         ],
+
     ],
+
 ];
