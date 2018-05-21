1f76fd9648cc5cf74699cbf1617fca6659f627fb
irfinhandi <irfinhandi3@gmail.com>
2018-05-21 17:10:29 +0700

    Update JatahCutiTableSeeder.php

--- laravel/database/seeds/JatahCutiTableSeeder.php
+++ laravel/database/seeds/JatahCutiTableSeeder.php
@@ -30,5 +30,15 @@ class JatahCutiTableSeeder extends Seeder
         'sisa_cuti'=>'12',
         'is_berlaku'=>'1',
         ]);
+        
+         DB::table('jatah_cuti')->insert([
+        'id_jenis'=> '6',
+        'id_employee'=>'4',
+        'tanggal_awal'=>'2017-12-25',
+        'tanggal_akhir'=>'2018-12-25',
+    	'tanggal_hangus'=>'2018-06-25',
+    	'sisa_cuti'=>'12',
+    	'is_berlaku'=>'1',
+    	]);
     }
 }
