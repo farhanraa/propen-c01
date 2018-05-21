80342288fa9954eef8742dd60c079a4e9b3e512b
irfinhandi <irfinhandi3@gmail.com>
2018-05-21 18:55:19 +0700

    Update JatahCutiTableSeeder.php

--- laravel/database/seeds/JatahCutiTableSeeder.php
+++ laravel/database/seeds/JatahCutiTableSeeder.php
@@ -11,34 +11,51 @@ class JatahCutiTableSeeder extends Seeder
      */
     public function run()
     {
-         DB::table('jatah_cuti')->insert([
-        'id_jenis'=> '1',
-        'id_employee'=>'1',
-        'tanggal_awal'=>'2018-02-15',
-        'tanggal_akhir'=>'2019-02-15',
-        'tanggal_hangus'=>'2019-08-15',
-        'sisa_cuti'=>'12',
-        'is_berlaku'=>'1',
-        ]);
-
-        DB::table('jatah_cuti')->insert([
-        'id_jenis'=> '6',
-        'id_employee'=>'2',
-        'tanggal_awal'=>'2017-12-25',
-        'tanggal_akhir'=>'2018-12-25',
-        'tanggal_hangus'=>'2018-06-25',
-        'sisa_cuti'=>'12',
-        'is_berlaku'=>'1',
-        ]);
-        
-         DB::table('jatah_cuti')->insert([
-        'id_jenis'=> '6',
-        'id_employee'=>'4',
-        'tanggal_awal'=>'2017-12-25',
-        'tanggal_akhir'=>'2018-12-25',
-    	'tanggal_hangus'=>'2018-06-25',
-    	'sisa_cuti'=>'12',
-    	'is_berlaku'=>'1',
-    	]);
+             DB::table('jatah_cuti')->insert([
+            'id_jenis'=> '1',
+            'id_employee'=>'1',
+            'tanggal_awal'=>'2018-02-15',
+            'tanggal_akhir'=>'2019-02-15',
+            'tanggal_hangus'=>'2019-08-15',
+            'sisa_cuti'=>'12',
+            'is_berlaku'=>'1',
+            ]);
+               
+              DB::table('jatah_cuti')->insert([
+            'id_jenis'=> '1',
+            'id_employee'=>'2',
+            'tanggal_awal'=>'2018-02-15',
+            'tanggal_akhir'=>'2019-02-15',
+            'tanggal_hangus'=>'2019-08-15',
+            'sisa_cuti'=>'12',
+            'is_berlaku'=>'1',
+            ]);
+			DB::table('jatah_cuti')->insert([
+            'id_jenis'=> '1',
+            'id_employee'=>'3',
+            'tanggal_awal'=>'2018-02-15',
+            'tanggal_akhir'=>'2019-02-15',
+            'tanggal_hangus'=>'2019-08-15',
+            'sisa_cuti'=>'12',
+            'is_berlaku'=>'1',
+            ]);
+			DB::table('jatah_cuti')->insert([
+            'id_jenis'=> '1',
+            'id_employee'=>'4',
+            'tanggal_awal'=>'2018-02-15',
+            'tanggal_akhir'=>'2019-02-15',
+            'tanggal_hangus'=>'2019-08-15',
+            'sisa_cuti'=>'12',
+            'is_berlaku'=>'1',
+            ]);
+			DB::table('jatah_cuti')->insert([
+            'id_jenis'=> '1',
+            'id_employee'=>'5',
+            'tanggal_awal'=>'2018-02-15',
+            'tanggal_akhir'=>'2019-02-15',
+            'tanggal_hangus'=>'2019-08-15',
+            'sisa_cuti'=>'12',
+            'is_berlaku'=>'1',
+            ]);
     }
 }
